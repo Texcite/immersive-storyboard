4094fcc93cab317bc5be5c3314c5fbe80ff7f506
Stefan de Gier <stefan_de_gier@outlook.com>
2023-03-10 11:56:22 +0100

    Added example code headlessUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import DemoTab from './components/demoTab'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <DemoTab></DemoTab>
+    </>
   )
 }
