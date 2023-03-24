b654ee988dfbc785d3ea7c96ba75095f526b6831
nigelritfeld <nigelritfeld@live.nl>
2023-03-24 13:18:58 +0100

    TEX-82: Refacored code

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,9 +1,10 @@
-import { NextPageWithLayout } from "@/pages/_app";
+import {NextPageWithLayout} from "@/pages/_app";
 import BaseLayout from "@/layouts/base";
 import DashboardLayout from "@/layouts/DashboardLayout";
-import { ReactElement, useState } from "react";
-
-import ReactPlayer from 'react-player'
+import {ReactElement, useState} from "react";
+import dynamic from 'next/dynamic'
+import {PauseIcon, PlayIcon} from "@heroicons/react/20/solid";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 // TODO Delete before merge
 // Create a panel type for the panels array
@@ -29,14 +30,13 @@ const panels: Array<Panel> = [
     }
 ]
 
-const Timeline: NextPageWithLayout = ({ }) => {
-
+const Timeline: NextPageWithLayout = ({}) => {
     const [video, setVideo] = useState<Panel>(panels[0]);
     const [playing, setPlaying] = useState<boolean>(true);
     const [duration, setDuration] = useState<number>(0);
     const [totalProgress, setTotalProgress] = useState<number>(0);
     const [progress, setProgress] = useState<number>(0);
-
+    const seconds = `${Math.floor(progress / 60)}:${Math.floor(progress % 60) < 10 ? "0" : ""}${Math.floor(progress % 60)}`
     const handleEnd = () => {
         const currentPanelIndex = panels.findIndex((panel) => panel === video);
         const nextPanelIndex = currentPanelIndex + 1;
@@ -48,41 +48,41 @@ const Timeline: NextPageWithLayout = ({ }) => {
     };
 
     return (
-        <>
-            <h1>content</h1>
-            <h1>Video player</h1>
-            <h1>Timeline</h1>
-            <div className="w-full bg-black flex flex-col items-center justify-center rounded-xl" >
-                <ReactPlayer muted
-                    playing={playing}
-                    url={video.video}
-                    width="75%"
-                    height={360}
-                    onProgress={(played) => {
+        <section className="w-full bg-black flex flex-col items-center justify-center rounded-xl">
+            <ReactPlayer
+                muted
+                playing={playing}
+                url={video.video}
+                width="75%"
+                height={360}
+                onProgress={
+                    (played) => {
                         setProgress(totalProgress + played.playedSeconds)
+                    }
+                }
+                onEnded={() => handleEnd()}
+            />
+            <div className=" py-2 w-full bg-[#1e1e1e] flex items-center justify-center relative rounded-b-xl">
+                <p className="text-white absolute left-8">
+                    <span className='font-semibold'>{seconds}</span>
+                     <span className="font-light"> / {`${Math.floor(duration / 60)}:${Math.floor(duration % 60) < 10 ? "0" : ""}${Math.floor(duration % 60)}`}</span>
+                </p>
+                <button
+                    className="bg-[#23A6F0] text-white font-bold rounded-full  h-10 w-10  flex items-center justify-center "
+                    onClick={() => {
+                        setPlaying(!playing)
                     }}
-                    onEnded={handleEnd}
-                />
-                <div className="h-8 w-full bg-[#1e1e1e] flex items-center justify-center pl-4 relative rounded-b-xl">
-                    <p className="text-white absolute left-8">
-                        {`${Math.floor(progress / 60)}:${Math.floor(progress % 60) < 10 ? "0" : ""}${Math.floor(progress % 60)}/${Math.floor(duration / 60)}:${Math.floor(duration % 60) < 10 ? "0" : ""}${Math.floor(duration % 60)}`}
-                    </p>
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full "
-                        onClick={() => { setPlaying(!playing) }}
-                    >
-                        {playing ?
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"> <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" /> </svg>
-                            :
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"> <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" /></svg>
-                        }
-                    </button>
-                </div>
+                >
+                    {playing ?
+                       <PauseIcon className="w-4 h-4"/>
+                        :
+                        <PlayIcon className="w-4 h-4"/>
+                    }
+                </button>
             </div>
-        </>
+        </section>
     )
 }
-
 
 Timeline.getLayout = function (page: ReactElement) {
     return <BaseLayout>
