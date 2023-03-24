005f892ded0f77ba493b935d76c8b643f8c89ccd
Steef van Deventer <svdev0310@gmail.com>
2023-03-24 12:31:41 +0100

    TEX-100-WIP-ALMOST_FUNCTIONAL

--- components/form/options.tsx
+++ components/form/options.tsx
@@ -3,12 +3,31 @@ import { classNames } from "@/utils/classNames";
 
 export default function Options(props: any) {
 
-    var posibleOptions = props.propOptions
+    var posibleOptions = props.availableOptions
+
+    function changeSelected(value: any){
+
+        posibleOptions.options.map((option: any ) => {
+
+            if (option.name === value.name){
+                option.classes = 'selected rounded bg-blue-500 inline-block'
+                props.changeSelected({[props.stateVariable]: option.value})
+            } else {
+                option.classes = 'rounded bg-blue-500 inline-block'
+            }
+
+        })
+    }
 
     return (
         <div>
             <h2>{props.label}</h2>
-            <p>{props.availableOptions}</p>
+            <div className="space-x-4">
+            {posibleOptions.options.map((option: any, key: any) => (
+                
+        <div key={key} className={option.classes} onClick={() => changeSelected(option)} >{option.name}</div>
+      ))}
+      </div>
         </div>
     );
 }
--- layouts/BuilderLayout.tsx
+++ layouts/BuilderLayout.tsx
@@ -3,8 +3,37 @@ import { classNames } from "@/utils/classNames";
 import Button from "@/components/form/button";
 import TextInput from "@/components/form/inputField";
 import Options from "@/components/form/options";
+import { useEffect, useMemo, useState } from "react";
 
 export default function BuilderLayout() {
+
+    const [panelVars, setPanelState] = useState({
+
+        title: "test",
+        comment: "dit is een test",
+        style: "realistic",
+        cameraZoom: "Top-left zoom",
+        perspective: "optie 1",
+        POV: "first person"
+
+
+    });
+
+    function changeState(newObject: any){
+        setPanelState({
+            title: newObject.title || panelVars.title,
+            comment: newObject.comment || panelVars.comment,
+            style: newObject.style || panelVars.style,
+            cameraZoom: newObject.cameraZoom || panelVars.cameraZoom,
+            perspective: newObject.perspective || panelVars.perspective,
+            POV: newObject.POV || panelVars.POV
+        })
+
+    }
+
+    useEffect(() => {
+        console.log(panelVars)
+    })
 
     function backHandler() {
         window.location.href = '/timeline';
@@ -16,10 +45,32 @@ export default function BuilderLayout() {
 
             <h1>The scene:</h1>
 
-            <Options label="label" availableOptions={['option1', 'option2', 'option3']} stateVariable="perspective" />
+            <Options label="label" availableOptions={testList} stateVariable="perspective" changeSelected={changeState} />
 
         </div>
     );
+}
+
+var testList = {
+
+    options: [
+
+    {
+        name: 'optie 1',
+        value: 'optie_1',
+        classes: 'selected rounded bg-blue-500 inline-block',
+    },
+    {
+        name: 'optie 2',
+        value: 'optie_2',
+        classes: 'rounded bg-blue-500 inline-block',    
+    },
+    {
+        name: 'optie 3',
+        value: 'optie_3',
+        classes: 'rounded bg-blue-500 inline-block',    
+    },
+    ]
 }
 
 
