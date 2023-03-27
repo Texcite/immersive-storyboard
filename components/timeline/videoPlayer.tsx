731d7a218744512c2ac149e1bb66c8a59c17f534
nigelritfeld <nigelritfeld@live.nl>
2023-03-27 22:25:10 +0200

    TEX-91: Added blue border to active panel preview image

--- components/timeline/videoPlayer.tsx
+++ components/timeline/videoPlayer.tsx
@@ -22,8 +22,7 @@ export const VideoPlayer:FC<VideoPlayerProps> = ({panels, setCurrent}) => {
         const currentPanelIndex = panels.findIndex((panel) => panel === video);
         const nextPanelIndex = currentPanelIndex + 1;
         setCurrent(currentPanelIndex)
-        const nextPanel =
-            nextPanelIndex < panels.length ? panels[nextPanelIndex] : panels[0];
+        console.log(currentPanelIndex)
         const nextPanel = nextPanelIndex < panels.length ? panels[nextPanelIndex] : panels[0];
 
         if (nextPanelIndex === panels.length) {
